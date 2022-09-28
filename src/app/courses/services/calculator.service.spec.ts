8e177c21d39bfca65603016ec77c9296926853de
Chad Pantin <cpantin@laisar.com>
2022-09-28 11:49:09 -0400

    SectionOneComplete

---
+++ src/app/courses/services/calculator.service.spec.ts
@@ -0,0 +1,46 @@
+import { TestBed } from "@angular/core/testing";
+import { CalculatorService } from "./calculator.service";
+import { LoggerService } from "./logger.service";
+
+
+describe('CalculatorService', () =>{
+    let calculator:CalculatorService
+    let loggerSpy: any;
+
+    //Setup the tests
+    beforeEach(()=>{
+            //Jasmine spies allow us to intecept a call/mock a function 
+            loggerSpy = jasmine.createSpyObj('LoggersService', ["log"]);
+
+            //Mock all dependencies
+            TestBed.configureTestingModule({
+                providers:[
+                    CalculatorService,
+                    {provide: LoggerService, useValue:loggerSpy}
+                ]
+            });
+
+            calculator = TestBed.inject(CalculatorService)
+    });
+
+    xit('should add two numbers',() =>{
+
+        //Preform the tests
+        const result = calculator.add(2,2);
+
+        //Run a series of assertions that either fail or pass the test
+        expect(result).toBe(4);
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+
+    it('should subtract two numbers',() =>{
+       
+
+        const result = calculator.subtract(2,2);
+
+        expect(result).toBe(0, "Unexpected subtraction result");
+
+        expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+    });
+});
