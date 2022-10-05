d93a014700f861400c9bc3499170df4ec876b44c
Chad Pantin <cpantin@laisar.com>
2022-10-05 08:50:20 -0400

    Section5Init

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,5 +1,15 @@
 describe('Home Page',()=>{
+
     it('should display a list of courses', ()=>{
-        expect(true).to.equal(true)
-    })
+        cy.fixture('courses.json').as("coursesJSON");
+        //mock returned data
+        cy.server()
+        cy.route('/api/courses', "@coursesJSON").as("courses")
+        
+        cy.visit('localhost:4401');
+        cy.contains("All Courses");
+
+        cy.wait('@courses');
+        cy.get("mat-card").should("have.length", 9)
+    });
 })
